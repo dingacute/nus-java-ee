8672291ef343c12ded78bbdc6d82b07c410aaee6
ChenLing <majarling@hotmail.com>
2018-11-13 16:48:17 +0800

    Day 1 & Day 2 upload test

---
+++ workshop01/src/java/workshop01/web/WeatherServlet.java
@@ -0,0 +1,100 @@
+package workshop01.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javafx.scene.text.Text;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+
+@WebServlet(urlPatterns = { "/weather" })
+public class WeatherServlet extends HttpServlet{
+
+    private static final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather";
+    private static final String APPID = "2ed6d71a7125ee4f69aadcadcc508d6a";
+    private Client client;
+    
+    //Initialize the client
+    @Override
+    public void init()
+            throws ServletException {        
+        client = ClientBuilder.newClient();
+    }
+
+    //Cleanup
+    @Override
+    public void destroy() {
+        client.close();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Form field name
+        String cityName = req.getParameter("cityName");
+        
+        //if no input city name, default is Singapore
+        //.trim will remove all the white spaces at the sides
+        if ((cityName == null) || (cityName.trim().length() <= 0)) {
+            cityName = "Singapore";
+        }
+        
+        //Start to setup the resource part
+        //Create the target
+        WebTarget target = client.target(WEATHER_URL);
+        //Set the query string
+        target = target.queryParam("q", cityName);
+        target = target.queryParam("appid", APPID);
+        target = target.queryParam("units", "metric");
+        
+        //or you can chain all them together
+        /*WebTarget target = client.target(WEATHER_URL)
+                .queryParam("q", cityName)
+                .queryParam("appid", APPID)
+                .queryParam("units", "metric");*/
+        
+        //Create invocation, expect JSON as result
+        Invocation.Builder inv = target.request(MediaType.APPLICATION_JSON);
+        //Complete the setup of the resource part
+        
+        //Make the call using GET HTTP method
+        //result will be null if there are error
+        JsonObject result = inv.get(JsonObject.class);
+        JsonArray weatherDetails = result.getJsonArray("weather");
+        
+        log("RESULT: " + result);
+        
+        //Echo back the name
+        //Set the 200 OK status code
+        resp.setStatus(HttpServletResponse.SC_OK);
+        
+        //Set the content type/media type
+        //We are returning text/html
+        resp.setContentType(MediaType.TEXT_HTML);
+        
+        try (PrintWriter pw = resp.getWriter()) {
+            pw.print("<h2>The weather for " + cityName.toUpperCase() + "</h2>");
+            for (int i = 0; i < weatherDetails.size(); i++) {
+                JsonObject wd = weatherDetails.getJsonObject(i);
+                String main = wd.getString("main");
+                String description = wd.getString("description");
+                String icon = wd.getString("icon");//an image for this weather
+                pw.print("<div>");
+                //pw.printf("%s &dash; %s", main, description);//inside the html, type &dash instead of real dash '-'
+                pw.print(main + " &dash; " + description);
+                pw.printf("<img src=\"http://openweathermap.org/img/w/%s.png\">", icon);
+                pw.print("</div>");
+            }
+        }
+    }
+    
+    
+}
---
+++ workshop02/src/java/workshop02/model/Customer.java
@@ -0,0 +1,129 @@
+package workshop02.model;
+
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Customer {
+    
+    @Id @Column(name = "customer_id")
+    private Integer customerId;
+       
+    private String name;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String zip;
+    private String phone;
+    private String fax;  
+    private String email;   
+    
+    @Column(name = "credit_limit")
+    private Integer creditLimit;
+    
+    @ManyToOne
+    @JoinColumn(name = "discount_code", 
+            referencedColumnName = "discount_code")
+    private DiscountCode discountCode;
+
+    @OneToMany(mappedBy = "customer")
+    private List<PurchaseOrder> purchaseOrders;
+    
+    public Integer getCustomerId() {
+        return customerId;
+    }
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddressLine1() {
+        return addressLine1;
+    }
+    public void setAddressLine1(String addressLine1) {
+        this.addressLine1 = addressLine1;
+    }
+
+    public String getAddressLine2() {
+        return addressLine2;
+    }
+    public void setAddressLine2(String addressLine2) {
+        this.addressLine2 = addressLine2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getCreditLimit() {
+        return creditLimit;
+    }
+    public void setCreditLimit(Integer creditLimit) {
+        this.creditLimit = creditLimit;
+    }
+    
+    public DiscountCode getDiscountCode() {
+        return discountCode;
+    }
+    public void setDiscountCode(DiscountCode discountCode) {
+        this.discountCode = discountCode;
+    }
+
+    public List<PurchaseOrder> getPurchaseOrders() {
+        return purchaseOrders;
+    }
+    public void setPurchaseOrders(List<PurchaseOrder> purchaseOrders) {
+        this.purchaseOrders = purchaseOrders;
+    }    
+    
+}
